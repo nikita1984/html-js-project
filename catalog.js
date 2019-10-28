2d241c45ac16b414179338eaba3cd10f86b75945
nikita1984 <nikitabessonov.ru@gmail.com>
2019-10-28 19:23:10 +0300

    реализована отрисовка vue-компонента <cart-list-component></cart-list-component>

--- catalog.js
+++ catalog.js
@@ -43,9 +43,10 @@ const CatalogListComponent = {
 
 const CartListComponent = {
     props: [],
-    template: `<div>     
-                    <div class="sh-count">5</div>
-                    <div class="shopping-cart__drop">
+    template: `<div class="sh"><a href="shopping-card.htm"><img src="img/Forma_1.svg" alt="basket"
+                                                                     class="shopping-cart"></a>
+                        <div class="sh-count">5</div>
+                        <div class="shopping-cart__drop">
                             <div class="product-in-sc">
                                 <a href="single-page.htm" style="float: left; width: 240px;">
                                     <div class="product-in-sc-img" style="background-image: url(img/sc_img1.jpg)">
@@ -95,11 +96,8 @@ const CartListComponent = {
                             <a href="checkout.htm" class="sc-btn">Checkout</a>
                             <a href="shopping-card.htm" class="sc-btn">go&nbsp;to&nbsp;cart</a>
                         </div>
-                    </div>
-              </div>`,
-    components: {
-
-    }
+                    </div>`,
+    components: {}
 };
 
 const app = new Vue({
