2eed4470b0ef03ac4cbd433fe6d3369df6bc01f7
nikita1984 <nikitabessonov.ru@gmail.com>
2019-10-28 19:16:44 +0300

    без коментария

--- catalog.js
+++ catalog.js
@@ -1,6 +1,6 @@
-    const CatalogComponent = {
-        props: ['id', 'title', 'price', 'img'],
-        template: `<div class="catalog-flex">
+const CatalogComponent = {
+    props: ['id', 'title', 'price', 'img'],
+    template: `<div class="catalog-flex">
                         <a href="single-page.htm" class="productUnit">
                             <div class="unit-img" :style="img"></div>
                             <div class="unit-text">
@@ -19,11 +19,11 @@
                         <a href="#" class="addtocard2"><i class="far fa-heart"></i></a>
                         <a href="#" class="addtocard3"><i class="fas fa-retweet"></i></a>
                     </div>`
-    };
+};
 
-    const CatalogListComponent = {
-      props: ['items'],
-      template: `<div class="product-catalog">
+const CatalogListComponent = {
+    props: ['items'],
+    template: `<div class="product-catalog">
             <catalog-component 
             v-if="items.length"
             v-for="item in items"
@@ -35,25 +35,88 @@
             @buy="handleBuyClick(item)"
             ></catalog-component>            
           </div>`,
-      components: {
+    components: {
         'catalog-component': CatalogComponent,
-      }
-    };
+    }
+};
 
-    const app = new Vue ({
-      el: '#root',
-      data: {
+
+const CartListComponent = {
+    props: [],
+    template: `<div>     
+                    <div class="sh-count">5</div>
+                    <div class="shopping-cart__drop">
+                            <div class="product-in-sc">
+                                <a href="single-page.htm" style="float: left; width: 240px;">
+                                    <div class="product-in-sc-img" style="background-image: url(img/sc_img1.jpg)">
+                                    </div>
+
+                                    <div class="product-in-sc-desc">
+                                        <h3 class="h3-sc-name">Rebox Zane</h3>
+                                        <div class="sc-rating">
+                                            <i class="fas fa-star rat"></i>
+                                            <i class="fas fa-star rat"></i>
+                                            <i class="fas fa-star rat"></i>
+                                            <i class="fas fa-star rat"></i>
+                                            <i class="fas fa-star rat"></i>
+                                        </div>
+                                        <div class="sc-count">1&nbsp;x $250</div>
+
+                                    </div>
+                                </a>
+                                <div class="sh__action"><a href="#" class="action"><i
+                                        class="far fa-times-circle"></i></a></div>
+
+                            </div>
+                            <div class="product-in-sc"><a href="single-page.htm" style="float: left; width: 240px;">
+                                <div class="product-in-sc-img" style="background-image: url(img/sc_img2.jpg)">
+                                </div>
+
+                                <div class="product-in-sc-desc">
+                                    <h3 class="h3-sc-name">Rebox Zane</h3>
+                                    <div class="sc-rating">
+                                        <i class="fas fa-star rat"></i>
+                                        <i class="fas fa-star rat"></i>
+                                        <i class="fas fa-star rat"></i>
+                                        <i class="fas fa-star rat"></i>
+                                        <i class="fas fa-star rat"></i>
+                                    </div>
+                                    <div class="sc-count">1&nbsp;x $250</div>
+
+                                </div>
+                            </a>
+                                <div class="sh__action"><a href="#" class="action"><i
+                                        class="far fa-times-circle"></i></a></div>
+                            </div>
+                            <div class="total">
+                                <div>total</div>
+                                <div>$500.00</div>
+                            </div>
+                            <a href="checkout.htm" class="sc-btn">Checkout</a>
+                            <a href="shopping-card.htm" class="sc-btn">go&nbsp;to&nbsp;cart</a>
+                        </div>
+                    </div>
+              </div>`,
+    components: {
+
+    }
+};
+
+const app = new Vue({
+    el: '#root',
+    data: {
         items: [],
-      },
-      mounted() {
+    },
+    mounted() {
         fetch('/goods')
             .then(response => response.json())
             .then((goods) => {
-              this.items = goods;
+                this.items = goods;
             });
-      },
-      components: {
+    },
+    components: {
         'catalog-list-component': CatalogListComponent,
-      },
-    });
+        'cart-list-component': CartListComponent
+    },
+});
 
