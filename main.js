a7169f99414f2114e94e31639ff5dc71bf29f774
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-03-08 10:59:25 +0300

    realise rendering of sc-page-list in shoping-cart page: final_stage

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const SCListElement = {
+    props: ['id', 'title', 'qty', 'price', 'img', 'color', 'size', 'shipping'],
     template: `<div class="unit-shopping-card">
-
                     <a href="single-page.htm" class="product-details shopping-card-unit">
-                        <div style="background-image: url(img/shopping-card-img/Layer_43.jpg);"
+                        <div :style="img"
                              class="unit-shopping-card-img"></div>
                         <div class="unit-shopping-card-info">
-                            <div class="unit-shopping-card-name">Mango People T-shirt</div>
+                            <div class="unit-shopping-card-name">{{title}}</div>
                             <div class="unit-shopping-card-rating">
                                 <i class="fas fa-star rat"></i>
                                 <i class="fas fa-star rat"></i>
@@ -13,15 +13,15 @@ const SCListElement = {
                                 <i class="fas fa-star rat"></i>
                                 <i class="fas fa-star rat"></i>
                             </div>
-                            <div class="unit-shopping-card-color"><span class="color-size">Color:</span> Red</div>
-                            <div class="unit-shopping-card-color"><span class="color-size">Size:</span> Xll</div>
+                            <div class="unit-shopping-card-color"><span class="color-size">Color:</span> {{color}}</div>
+                            <div class="unit-shopping-card-color"><span class="color-size">Size:</span> {{size}}</div>
                         </div>
                     </a>
 
                     <div class="flex-shopping-card">
-                        <div class="shopping-card-price">$150</div>
-                        <div><input type="number" placeholder="2" class="quanity"></div>
-                        <div class="shipping-shopping-card">free</div>
+                        <div class="shopping-card-price">$ {{price}}</div>
+                        <div><input type="number" :placeholder="qty" class="quanity"></div>
+                        <div class="shipping-shopping-card">{{shipping}}</div>
                         <div class="shopping-card-price">$300</div>
                         <div><a href="#" class="action"><i class="far fa-times-circle"></i></a></div>
                     </div>
@@ -29,6 +29,7 @@ const SCListElement = {
 };
 
 const SСListComponent = {
+    props: ['items'],
     template: `<div class="shopping-card container">
                 <div class="header__shopping-card">
                     <div class="product-details">Product Details</div>
@@ -40,66 +41,20 @@ const SСListComponent = {
                         <div>ACTION</div>
                     </div>
                 </div>
-
-                <sc-list-element></sc-list-element>
-
-                <div class="unit-shopping-card">
-                    <a href="single-page.htm" class="product-details shopping-card-unit">
-                        <div style="background-image: url(img/shopping-card-img/Layer_44.jpg);"
-                             class="unit-shopping-card-img"></div>
-                        <div class="unit-shopping-card-info">
-                            <div class="unit-shopping-card-name">Mango People T-shirt</div>
-                            <div class="unit-shopping-card-rating">
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                            </div>
-                            <div class="unit-shopping-card-color"><span class="color-size">Color:</span> Red</div>
-                            <div class="unit-shopping-card-color"><span class="color-size">Size:</span> Xll</div>
-                        </div>
-                    </a>
-                    <div class="flex-shopping-card">
-                        <div class="shopping-card-price">$150</div>
-                        <div><input type="number" placeholder="2" class="quanity"></div>
-                        <div class="shipping-shopping-card">free</div>
-                        <div class="shopping-card-price">$300</div>
-                        <div><a href="#" class="action"><i class="far fa-times-circle"></i></a></div>
-                    </div>
-                </div>
-
-                <div class="unit-shopping-card">
-                    <a href="single-page.htm" class="product-details shopping-card-unit">
-                        <div style="background-image: url(img/shopping-card-img/Layer-45.jpg);"
-                             class="unit-shopping-card-img"></div>
-                        <div class="unit-shopping-card-info">
-                            <div class="unit-shopping-card-name">Mango People T-shirt</div>
-                            <div class="unit-shopping-card-rating">
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                                <i class="fas fa-star rat"></i>
-                            </div>
-                            <div class="unit-shopping-card-color"><span class="color-size">Color:</span> Red</div>
-                            <div class="unit-shopping-card-color"><span class="color-size">Size:</span> Xll</div>
-                        </div>
-                    </a>
-                    <div class="flex-shopping-card">
-                        <div class="shopping-card-price">$150</div>
-                        <div><input type="number" placeholder="2" class="quanity"></div>
-                        <div class="shipping-shopping-card">free</div>
-                        <div class="shopping-card-price">$300</div>
-                        <div><a href="#" class="action"><i class="far fa-times-circle"></i></a></div>
-                    </div>
-                </div>
-
+                <sc-list-element v-for="item in items"
+                              :key="item.id"
+                              :title="item.title"
+                              :id="item.id"
+                              :qty="item.qty"
+                              :price="item.price"
+                              :img="item.cartPageImage"
+                              :color="item.color"
+                              :size="item.size"
+                              :shipping="item.shipping"></sc-list-element>
                 <div class="shopping-card-buttons">
                     <div><a href="#" class="btn">cLEAR SHOPPING CART</a></div>
                     <div><a href="checkout.htm" class="btn">cONTINUE sHOPPING</a></div>
                 </div>
-
             </div>`,
     components: {
         'sc-list-element': SCListElement
@@ -144,7 +99,7 @@ const CartListComponent = {
                               :id="item.id"
                               :qty="item.qty"
                               :price="item.price"
-                              :img="item.cartdropmenuimg">
+                              :img="item.cartDropMenuImage">
                             </cart-component>
                             <div class="total">
                                 <div>total</div>
