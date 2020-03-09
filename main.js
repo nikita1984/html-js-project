1ad776ade32d0d03688d47e07c4ad5049678f8c6
nikita1984 <52231270+nikita1984@users.noreply.github.com>
2020-03-09 09:05:05 +0300

    add header menu in index.htm: stage: 1

--- main.js
+++ main.js
@@ -193,6 +193,51 @@ const CatalogListComponent = {
     }
 };
 
+const HeaderComponent = {
+  template: `<header class="header">
+            <div class="container header__flex">
+                <div class="header__left">
+                    <a class="logo" href="index.htm"><img src="img/logo.png" alt="logo">
+                        <div class="brand">BRAN<span class="pink weight">D</span></div>
+                    </a>
+                    <form class="header__form" action="#">
+                        <div class="browse"><a href="#" class="brw">Browse<i class="fas fa-caret-down"></i></a>
+                            <div class="drop__browse">
+                                <h3 class="drop__header">Women</h3>
+                                <ul>
+                                    <li><a href="catalog.htm" class="drop__link">Dresses</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Tops</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Sweaters/Knits</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Jackets/Coats</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Blazers</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Denim</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Leggings/Pants</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Skirts/Shorts</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Accessories </a></li>
+                                </ul>
+                                <h3 style="margin-top: 22px;" class="drop__header">Men</h3>
+                                <ul>
+                                    <li><a href="catalog.htm" class="drop__link">Tees/Tank tops</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Shirts/Polos</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Sweaters</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Sweatshirts/Hoodies</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Blazers</a></li>
+                                    <li><a href="catalog.htm" class="drop__link">Jackets/vests</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <input type="text" class="search" placeholder="Search for Item...">
+                        <button class="search__button"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+                <div class="header__right">
+                   
+                    <a class="button" href="#">My&nbsp;Account<i class="fas fa-caret-down"></i></a>
+                </div>
+            </div>
+        </header>`
+};
+
 const app = new Vue({
     el: '#root',
     data: {
@@ -215,7 +260,8 @@ const app = new Vue({
     components: {
         'catalog-list-component': CatalogListComponent,
         'cart-list-component': CartListComponent,
-        'sc-list-component': SСListComponent
+        'sc-list-component': SСListComponent,
+        'header-component': HeaderComponent
     },
 });
 
